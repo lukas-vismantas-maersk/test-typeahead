959574a2e892f3baae1b79519dee98038997ab4d
lukas.vismantas <lukas.vismantas@maersk.com>
2022-02-21 09:21:37 +0100

    up

--- src/components/TypeAhead.vue
+++ src/components/TypeAhead.vue
@@ -1,18 +1,33 @@
 <template>
-  <div>
-    <mc-c-typeahead :options="options" />
-  </div>
+  <mc-c-typeahead
+    v-mc-model="selectedCountry"
+    :options.prop="options"
+    :label="label"
+    :placeholder="placeholder"
+    :value.prop="selectedCountry"
+  />
 </template>
 
-<script>
+<script lang="ts">
 import Vue from "vue";
 
 export default Vue.extend({
   data() {
     const options = ["Zero", "One", "Two", "Three", "Four", "Five"];
-    return { options };
+
+    return { options, selectedCountry: "" };
+  },
+  props: {
+    label: {
+      type: String,
+      default: "HCeck",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
